ca3367150a3f6947629bf18dc895ecb06fe402d7
chaya2z <chaya2z@users.noreply.github.com>
2022-07-05 00:47:28 +0000

    auto generated from https://github.com/CharVstack/CharV-OAS/pull/14

--- src/api-hooks/client/v1/models/index.ts
+++ src/api-hooks/client/v1/models/index.ts
@@ -1,5 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './GetApiV1Vm200Response';
-export * from './GetApiV1Vm200ResponseVmsInner';
-export * from './GetVmVmId200Response';
+export * from './GetApiV1Host200Response';
+export * from './GetApiV1Vms200Response';
+export * from './PatchApiV1VmsVmIdRequest';
+export * from './PostApiV1VmsRequest';
+export * from './StoragePool';
+export * from './Vm';
+export * from './VmDevices';
+export * from './VmDevicesDiskInner';
+export * from './VmMetadata';
