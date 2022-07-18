827bf0dbfc7d4ae0aa080faa344b3403c5d2cdbc
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2022-07-18 12:06:20 +0900

    Setup aspida

---
+++ src/env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_BACKEND_BASE_URL: string;
+  // その他の環境変数...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
