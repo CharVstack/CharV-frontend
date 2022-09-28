dfc3394abfce9e9d14ea4eebc44723ab311dccb6
chaya2z <chaya2zdev@gmail.com>
2022-09-29 04:29:55 +0900

    CVS-97 templates の削除

--- src/pages/ErrorPages/NotFound.test.tsx
+++ src/pages/ErrorPages/NotFound.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import { H1 } from '@components/atoms/Heading';
+import { H1 } from '@components/atoms/Headings';
 
 test('Output "NotFound"', () => {
   render(<H1>404 not found</H1>);
