4ed90cd67d7c8a4e918abd8a248acbb3d9de87b6
chaya2z <chaya2zdev@gmail.com>
2022-10-02 23:46:28 +0900

    CVS-27 /vms ページを作成

---
+++ src/pages/VMs/index.tsx
@@ -0,0 +1,5 @@
+import { Typography } from '@mui/material';
+
+export const VMs = () => {
+  return <Typography variant={'h2'}>VMs Page</Typography>;
+};
--- src/utils/Routing.tsx
+++ src/utils/Routing.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { Home, NotFound } from '@pages';
+import { Home, VMs, NotFound } from '@pages';
 
 export const Routing = () => (
   <BrowserRouter>
     <Routes>
       <Route index element={<Home />} />
+      <Route path="vms" element={<VMs />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
