32585b543f585fa66148d9a9cccb74b6de9139b5
chaya2z <chaya2zdev@gmail.com>
2022-10-20 14:02:52 +0900

    CVS-118 カードのレスポンシブ対応

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,23 +20,23 @@ export const Home = () => {
   ) : (
     <DashBoardLayout>
       <Container>
-        <Grid container spacing={4}>
-          <Grid item xs={6}>
+        <Grid container justifyContent="center" alignItems="center" spacing={4}>
+          <Grid item xs={12} sm={8} md={6}>
             {/* APIができていないため仮の値を指定 */}
             {/* ToDo: API叩いてデータ受け取るように */}
             <RunningVmsCard allVms={20} runningVms={16} />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={8} md={6}>
             <HostStatusCard
               cpuUsageRate={cpuUsageRate}
               memoryUsageRate={memoryUsageRate}
               storageUsageRate={storageUsageRate}
             />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={8} md={6}>
             <HostMemoryCard total={hostData.mem.total} used={hostData.mem.used} />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={8} md={6}>
             <HostStoragePoolCard {...hostData.storage_pools?.[0]} />
           </Grid>
         </Grid>
