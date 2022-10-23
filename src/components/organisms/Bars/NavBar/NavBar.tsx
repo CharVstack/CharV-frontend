4f358a36c77541fe125e13ef6e18f845255b0693
bluekey <kameyamatakeru12@gmail.com>
2022-10-23 16:51:29 +0900

    cvs-126 Sidebarの追加

--- src/components/organisms/Bars/NavBar/NavBar.tsx
+++ src/components/organisms/Bars/NavBar/NavBar.tsx
@@ -1,10 +1,10 @@
 import MenuIcon from '@mui/icons-material/Menu';
 import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 
-export const NavBar = () => (
+export const NavBar = ({ onClick }: { onClick: () => void }) => (
   <AppBar position="sticky">
     <Toolbar>
-      <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+      <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={onClick}>
         <MenuIcon />
       </IconButton>
       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
