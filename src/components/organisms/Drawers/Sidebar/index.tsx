30ea89f5908ed8717b0da4ebb92a216b81d4de95
bluekey <kameyamatakeru12@gmail.com>
2022-10-24 23:34:13 +0900

    Fix リンクをコンポーネントとして分割

--- src/components/organisms/Drawers/Sidebar/index.tsx
+++ src/components/organisms/Drawers/Sidebar/index.tsx
@@ -1,37 +1,17 @@
 import ComputerIcon from '@mui/icons-material/Computer';
 import HomeIcon from '@mui/icons-material/Home';
-import { Box, Drawer, Link, List, ListItem, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
+import { Box, Drawer, List } from '@mui/material';
 import { Dispatch, SetStateAction } from 'react';
 
-export const Sidebar = ({ isOpen, setIsOpen }: { isOpen: boolean; setIsOpen: Dispatch<SetStateAction<boolean>> }) => {
-  const textColor = useTheme();
-  const vms = '/vms';
-  return (
-    <Drawer open={isOpen} onClose={setIsOpen}>
-      <Box width={198}>
-        <List>
-          <Link href="/" color={textColor.palette.text.primary} underline="none">
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <HomeIcon />
-                </ListItemIcon>
-                <ListItemText primary="Home" />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-          <Link href={vms} color={textColor.palette.text.primary} underline="none">
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <ComputerIcon />
-                </ListItemIcon>
-                <ListItemText primary="VMs" />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-        </List>
-      </Box>
-    </Drawer>
-  );
-};
+import { DrawerLinkItem } from '@components/molecules/ListItems';
+
+export const Sidebar = ({ isOpen, setIsOpen }: { isOpen: boolean; setIsOpen: Dispatch<SetStateAction<boolean>> }) => (
+  <Drawer open={isOpen} onClose={setIsOpen}>
+    <Box width={198}>
+      <List>
+        <DrawerLinkItem path="/" name="Home" icon={<HomeIcon />} />
+        <DrawerLinkItem path="/vms" name="VMs" icon={<ComputerIcon />} />
+      </List>
+    </Box>
+  </Drawer>
+);
