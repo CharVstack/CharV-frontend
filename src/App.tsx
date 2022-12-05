4756e50f14e1ce746a8af6e27d68074153a276e9
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2022-12-05 11:16:48 +0900

    <component>/{index.ts,<component>.ts} を徹底する

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
 import { CssBaseline } from '@mui/material';
-import { Provider as JotaiProvider } from 'jotai';
-import { useAtomsDebugValue, useAtomsDevtools } from 'jotai/devtools';
 import { SnackbarProvider } from 'notistack';
 
 import { GlobalStyle, Theme } from '@styles';
 import { Routing } from '@utils/Routing';
 
-const DevAtoms = () => {
-  useAtomsDevtools('global');
-  return null;
-};
-
-const DebugAtoms = () => {
-  useAtomsDebugValue();
-  return null;
-};
-
 const App = () => (
-  <JotaiProvider>
-    <DebugAtoms />
-    <DevAtoms />
-    <SnackbarProvider maxSnack={4}>
-      <Theme>
-        <CssBaseline />
-        <GlobalStyle />
-        <Routing />
-      </Theme>
-    </SnackbarProvider>
-  </JotaiProvider>
+  <SnackbarProvider maxSnack={4}>
+    <Theme>
+      <CssBaseline />
+      <GlobalStyle />
+      <Routing />
+    </Theme>
+  </SnackbarProvider>
 );
 
 export default App;
