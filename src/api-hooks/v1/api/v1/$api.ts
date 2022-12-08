2c28014ce48a8bcb1f7d0d88034b1da3fa0730cb
gamoutatsumi <gamoutatsumi@users.noreply.github.com>
2022-12-08 02:04:12 +0000

    auto generated from https://github.com/CharVstack/CharV-OAS/pull/96

--- src/api-hooks/v1/api/v1/$api.ts
+++ src/api-hooks/v1/api/v1/$api.ts
@@ -1,5 +1,3 @@
-import { dataToURLString } from 'aspida';
-
 import type { Methods as Methods0 } from './host';
 import type { Methods as Methods1 } from './vms';
 import type { Methods as Methods2 } from './vms/_vmId@string';
@@ -41,13 +39,15 @@ const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
           power: {
             /**
              * VMの電源状態を操作
+             * @param option.body - VMの電源状態を操作
              */
-            post: (option?: { query?: Methods3['post']['query'] | undefined; config?: T | undefined } | undefined) =>
+            post: (option: { body: Methods3['post']['reqBody']; config?: T | undefined }) =>
               fetch<void, BasicHeaders, Methods3['post']['status']>(prefix, `${prefix1}${PATH2}`, POST, option).send(),
             /**
              * VMの電源状態を操作
+             * @param option.body - VMの電源状態を操作
              */
-            $post: (option?: { query?: Methods3['post']['query'] | undefined; config?: T | undefined } | undefined) =>
+            $post: (option: { body: Methods3['post']['reqBody']; config?: T | undefined }) =>
               fetch<void, BasicHeaders, Methods3['post']['status']>(prefix, `${prefix1}${PATH2}`, POST, option)
                 .send()
                 .then((r) => r.body),
@@ -75,8 +75,7 @@ const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
               )
                 .json()
                 .then((r) => r.body),
-            $path: (option?: { method: 'post'; query: Methods3['post']['query'] } | undefined) =>
-              `${prefix}${prefix1}${PATH2}${option && option.query ? `?${dataToURLString(option.query)}` : ''}`,
+            $path: () => `${prefix}${prefix1}${PATH2}`,
           },
           /**
            * 個々のVM情報を取得する
