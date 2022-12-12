c5af8a37f6b9387f411b738099648325b26d8e9f
bluekey <kameyamatakeru12@gmail.com>
2022-12-12 11:04:38 +0900

    CVS-193 VM情報取得時にエラーが発生した場合、ダイヤログが表示されるように修正

---
+++ src/components/organisms/Dialogs/HookError/HookErrorDialog.tsx
@@ -0,0 +1,70 @@
+import CloseIcon from '@mui/icons-material/Close';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import { useState } from 'react';
+
+const CustomizedDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+}));
+
+export interface DialogTitleProps {
+  id: string;
+  children?: React.ReactNode;
+  onClose: () => void;
+}
+
+export interface HookErrorDialogProp {
+  message: string;
+}
+
+const CustomizedDialogTitle = (props: DialogTitleProps) => {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 2,
+            top: 8,
+            color: (theme) => theme.palette.action.disabled,
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+};
+
+export const HookErrorDialog = ({ message }: HookErrorDialogProp) => {
+  const [open, setOpen] = useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <CustomizedDialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <CustomizedDialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Error
+        </CustomizedDialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>Failed to load: {message}</Typography>
+        </DialogContent>
+      </CustomizedDialog>
+    </div>
+  );
+};
--- src/pages/VMs/VMs.tsx
+++ src/pages/VMs/VMs.tsx
@@ -1,17 +1,28 @@
 import useAspidaSWR from '@aspida/swr';
-import { Container } from '@mui/material';
+import { Container, Stack } from '@mui/material';
+import { SWRResponse } from 'swr';
 
+import { Vm } from '@api-hooks/v1/@types';
 import { LoadingSpinner } from '@components/molecules/Progress';
 import { CreateNewVmButton } from '@components/organisms/Buttons';
 import { CreateVmDialog } from '@components/organisms/Dialogs';
+import { HookErrorDialog } from '@components/organisms/Dialogs/HookError/HookErrorDialog';
 import { InstanceTable } from '@components/organisms/Tables';
 import { apiClient } from '@lib/apiClient';
 import { DashBoard } from '@templates/DashBoard';
 
 export const VMs = () => {
-  const { data } = useAspidaSWR(apiClient.api.v1.vms);
+  const { data, error } = useAspidaSWR(apiClient.api.v1.vms) as SWRResponse<{ vms: Vm[] }, Error>;
 
   if (data === undefined) {
+    if (error) {
+      return (
+        <Stack>
+          <HookErrorDialog message="Could not retrieve VM information" />
+          <LoadingSpinner open />
+        </Stack>
+      );
+    }
     return <LoadingSpinner open />;
   }
 
