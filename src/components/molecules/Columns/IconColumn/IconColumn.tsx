821d91255b6d0b756cc1ad494270ec91d644c0b0
bluekey <kameyamatakeru12@gmail.com>
2022-12-13 18:48:47 +0900

    CVS-193 データが取れなかった時だけ UNKNOWN を表示させるように修正

--- src/components/molecules/Columns/IconColumn/IconColumn.tsx
+++ src/components/molecules/Columns/IconColumn/IconColumn.tsx
@@ -1,21 +1,7 @@
 import { Box, Stack, useTheme } from '@mui/material';
 
-import { HookErrorDialog } from '@components/organisms/Dialogs/HookError/HookErrorDialog';
-
-export const IconColumn = ({ icon, text, error }: { icon: JSX.Element; text: string; error: Error | undefined }) => {
+export const IconColumn = ({ icon, text }: { icon: JSX.Element; text: string }) => {
   const { spacing } = useTheme();
-
-  if (error) {
-    return (
-      <Stack>
-        <HookErrorDialog message="Could not retrieve VM power information" />
-        <Stack direction="row" alignItems="center" spacing={spacing(1)}>
-          {icon}
-          <Box>{text}</Box>
-        </Stack>
-      </Stack>
-    );
-  }
 
   return (
     <Stack direction="row" alignItems="center" spacing={spacing(1)}>
--- src/components/organisms/Columns/StatusColumn/StatusColumn.tsx
+++ src/components/organisms/Columns/StatusColumn/StatusColumn.tsx
@@ -4,17 +4,13 @@ import ErrorIcon from '@mui/icons-material/Error';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import { useTheme } from '@mui/material';
 import { GridRowId } from '@mui/x-data-grid/models/gridRows';
-import { SWRResponse } from 'swr';
 
-import { VmPowerInfo } from '@api-hooks/v1/@types';
 import { IconColumn } from '@components/molecules/Columns';
 import { apiClient } from '@lib/apiClient';
 
 export const StatusColumn = ({ rowId }: { rowId: GridRowId }) => {
-  const { data, error } = useAspidaSWR(apiClient.api.v1.vms._vmId(rowId as string).power) as SWRResponse<
-    { vm_power: VmPowerInfo },
-    Error
-  >;
+  const { data } = useAspidaSWR(apiClient.api.v1.vms._vmId(rowId as string).power);
+
   const status = data?.vm_power.state ?? 'UNKNOWN';
   const { palette } = useTheme();
 
@@ -30,5 +26,5 @@ export const StatusColumn = ({ rowId }: { rowId: GridRowId }) => {
     }
   })();
 
-  return <IconColumn icon={icon} text={status} error={error} />;
+  return <IconColumn icon={icon} text={status} />;
 };
