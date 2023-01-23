d34cdb2f5f1c758a96a99c5a47cdc3ba1d8c30cd
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2023-01-23 12:36:03 +0900

    不要なjotaiを削除

--- src/components/organisms/Drawers/SideBar/SideBar.tsx
+++ src/components/organisms/Drawers/SideBar/SideBar.tsx
@@ -1,20 +1,7 @@
 import { Computer as ComputerIcon, Home as HomeIcon } from '@mui/icons-material';
 import { Box, Drawer, List, Toolbar } from '@mui/material';
-import { atom, useAtomValue, useSetAtom, useAtom } from 'jotai';
 
 import { DrawerLinkItem } from '@components/molecules/ListItems';
-
-const baseAtom = atom(false);
-const sideBarAtom = atom<boolean, boolean>(
-  (get) => get(baseAtom),
-  (_get, set, newValue) => set(baseAtom, newValue)
-);
-const toggleSideBarAtom = atom(null, (get, set) => set(baseAtom, !get(baseAtom)));
-
-export const useSideBarWritableAtom = () => useAtom(sideBarAtom);
-export const useSideBarReadOnlyAtom = () => useAtomValue(sideBarAtom);
-export const useSideBarWriteOnlyAtom = () => useSetAtom(sideBarAtom);
-export const useToggleSideBarAtom = () => useSetAtom(toggleSideBarAtom);
 
 export const SideBar = () => (
   <Drawer variant="permanent" sx={{ zIndex: (theme) => theme.zIndex.drawer }}>
